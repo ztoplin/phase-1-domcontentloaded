3e3b229179c4ae6d601a41a97b4de2682fab38d5
Aaron <ztoplin@yahoo.com>
2022-10-13 20:05:01 -0400

    commit tree 91c648a386855e302156636ca4ead47ed82105f1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('#text').textContent = 'This is really cool!' 
+    console.log("The DOM has loaded");
+})
+
+console.log(
+    "Me First!"
+)
